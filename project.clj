1b0a2fb24bc39ab7cfddc82b5b2935349fb98bcb
Michiel Borkent <michielborkent@gmail.com>
2019-12-06 10:09:32 +0100

    codox

--- project.clj
+++ project.clj
@@ -12,6 +12,7 @@
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [borkdude/edamame "0.0.9-alpha.2"]
                  [org.clojure/tools.reader "1.3.2"]]
+  :plugins [[lein-codox "0.10.7"]]
   :profiles {:clojure-1.9.0 {:dependencies [[org.clojure/clojure "1.9.0"]]}
              :clojure-1.10.1 {:dependencies [[org.clojure/clojure "1.10.1"]]}
              :dev {:dependencies [[thheller/shadow-cljs "2.8.64"]]}
