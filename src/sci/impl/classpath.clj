64d354a39159503af4275c38e98784264fe9942b
Michiel Borkent <michielborkent@gmail.com>
2019-12-10 19:41:13 +0100

    wip

--- src/sci/impl/classpath.clj
+++ src/sci/impl/classpath.clj
@@ -1,31 +1,50 @@
 (ns sci.impl.classpath
   {:no-doc true}
-  (:require [clojure.java.io :as io]))
+  (:require [clojure.java.io :as io]
+            [clojure.string :as str])
+  (:import [java.util.jar JarFile JarFile$JarFileEntry]))
 
 (set! *warn-on-reflection* true)
 
-(defn file->url [f]
-  (-> (io/file f) .toURL))
+(defprotocol IResourceResolver
+  (getResource [this path]))
 
-(defn classpath->classloader [^String classpath]
+(deftype DirectoryResolver [path]
+  IResourceResolver
+  (getResource [this resource-path]
+    (let [f (io/file path resource-path)]
+      (when (.exists f)
+        (slurp f)))))
+
+(deftype JarFileResolver [path]
+  IResourceResolver
+  (getResource [this path]))
+
+(defn part->entry [part]
+  (if (str/ends-with? part ".jar")
+    (JarFileResolver. (JarFile. (io/file part)))
+    (DirectoryResolver. (io/file part))))
+
+(deftype Loader [entries]
+  IResourceResolver
+  (getResource [this resource-path]
+    (some #(getResource % resource-path) entries)))
+
+(defn classpath->entries [^String classpath]
   (let [parts (.split classpath (System/getProperty "path.separator"))
-        urls (map file->url parts)
-        urls (into-array java.net.URL urls)]
-    (java.net.URLClassLoader. urls)))
+        entries (map part->entry parts)]
+    (Loader. entries)))
 
-(defn get-resource-as-string [^java.net.URLClassLoader classloader path]
-  (when-let [is (.getResourceAsStream classloader path)]
-    (slurp is)))
-
-(defn source-for-namespace [^java.net.URLClassLoader classloader namespace]
+(defn source-for-namespace [loader namespace]
   (let [ns-str (name namespace)
         path (.replace ns-str "." "/")
         paths (map #(str path %) [".clj" ".cljc"])]
-    (some #(get-resource-as-string classloader %) paths)))
+    (some #(getResource loader %) paths)))
 
 ;;;; Scratch
 
 (comment
-  (def cl (classpath->classloader ".:file:///Users/borkdude/.m2/repository/cheshire/cheshire/5.9.0/cheshire-5.9.0.jar"))
-  (source-for-namespace cl 'cheshire.core)
+  (def loader (classpath->entries ".:file:///Users/borkdude/.m2/repository/cheshire/cheshire/5.9.0/cheshire-5.9.0.jar"))
+  (source-for-namespace loader 'src.sci.core)
+  (source-for-namespace loader 'cheshire.core)
   )
