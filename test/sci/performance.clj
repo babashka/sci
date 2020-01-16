cf360149b18269cbc4fd963ad83454be2846fe55
Michiel Borkent <michielborkent@gmail.com>
2020-01-16 16:42:51 +0100

    Add perf info

--- test/sci/performance.clj
+++ test/sci/performance.clj
@@ -10,6 +10,7 @@
 ;; c2ffce590f8185b9fd5fdbb7e3746ba37f2ae02f Execution time mean : 82.026964 µs
 ;; 33bc0b0a14d3f9d95ad9ecf9f1b29e95527153a3 Execution time mean : 48.430510 µs
 ;; d556416a1e932e334ffc77ac3c8158cb88d6165b Execution time mean : 38.172856 µs
+;; 08ed47722ccd65e60ca9f24991e94fe549ea0585 Execution time mean : 30.940262 µs
 
 (when (and (= "true" (System/getenv "SCI_TEST_PERFORMANCE")) (not native?))
   (deftest reusable-fn-test
