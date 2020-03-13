3d35fa61b21bcbbd3984dad8ee3b61d5bc93c189
Michiel Borkent <michielborkent@gmail.com>
2020-03-13 22:32:46 +0100

    Convert pointer into String

--- libsci/from-rust/src/main.rs
+++ libsci/from-rust/src/main.rs
@@ -7,7 +7,13 @@ use std::{env, ptr};
 
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 
-fn eval(expr: String) -> Result<&'static str, Utf8Error> {
+fn my_string_safe(ptr: *mut i8) -> String {
+    unsafe {
+        CStr::from_ptr(ptr).to_string_lossy().into_owned()
+    }
+}
+
+fn eval(expr: String) -> String {
     unsafe {
         let mut isolate: *mut graal_isolate_t = ptr::null_mut();
         let mut thread: *mut graal_isolatethread_t = ptr::null_mut();
@@ -19,16 +25,12 @@ fn eval(expr: String) -> Result<&'static str, Utf8Error> {
             CString::new(expr).expect("CString::new failed").as_ptr(),
         );
 
-        CStr::from_ptr(result).to_str()
+        my_string_safe(result)
     }
 }
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     let result = eval(args[1].to_owned());
-
-    match result {
-        Ok(output) => println!("{}", output),
-        Err(_) => println!("Failed."),
-    };
+    println!("{}", result)
 }
