aa73bdef358a089733f9e20b1673c551f3ccdd6b
Michiel Borkent <michielborkent@gmail.com>
2020-03-14 22:13:48 +0100

    Clean up resources after call to GraalVM (#292)

--- libsci/from-rust/src/main.rs
+++ libsci/from-rust/src/main.rs
@@ -7,7 +7,17 @@ use std::{env, ptr};
 
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 
-fn eval(expr: String) -> Result<&'static str, Utf8Error> {
+fn my_string_safe(ptr: *mut i8) -> Result<String,Utf8Error> {
+    let s = unsafe {
+        CStr::from_ptr(ptr).to_str()
+    };
+    match s {
+        Ok(s) => Ok(String::from(s)),
+        Err(x) => Err(x)
+    }
+}
+
+fn eval(expr: String) -> String {
     unsafe {
         let mut isolate: *mut graal_isolate_t = ptr::null_mut();
         let mut thread: *mut graal_isolatethread_t = ptr::null_mut();
@@ -18,17 +28,16 @@ fn eval(expr: String) -> Result<&'static str, Utf8Error> {
             thread as i64,
             CString::new(expr).expect("CString::new failed").as_ptr(),
         );
-
-        CStr::from_ptr(result).to_str()
+        let s = my_string_safe(result).unwrap();
+        graal_tear_down_isolate(thread);
+        // let s = my_string_safe(result).unwrap(); => segmentation fault, so it seems tear_down_isolate cleans stuff up.
+        // see https://github.com/oracle/graal/blob/master/substratevm/C-API.md
+        s
     }
 }
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     let result = eval(args[1].to_owned());
-
-    match result {
-        Ok(output) => println!("{}", output),
-        Err(_) => println!("Failed."),
-    };
+    println!("{}", result);
 }
