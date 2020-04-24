2ef58210be07b478170a51d393ad5fcdc850f376
Michiel Borkent <michielborkent@gmail.com>
2020-04-25 00:07:17 +0200

    [#312] fix interop with JS object

--- src/sci/impl/js.cljs
+++ src/sci/impl/js.cljs
@@ -1,7 +1,9 @@
 (ns sci.impl.js
   "JavaScript interface to sci."
   {:no-doc true}
-  (:require [sci.core :as sci]))
+  (:require
+   [goog.object :as gobject]
+   [sci.core :as sci]))
 
 (defn map-vals [f m]
   (reduce-kv (fn [m k v] (assoc m k (f v))) {} m))
@@ -9,28 +11,32 @@
 (defn map-keys [f m]
   (reduce-kv (fn [m k v] (assoc m (f k) v)) {} m))
 
-(defn symbolize-keys [m]
-  (map-keys symbol m))
+(defn obj->clj [x k-fn]
+  (persistent!
+   (reduce (fn [r k] (assoc! r (k-fn k) (gobject/get x k)))
+           (transient {}) (js-keys x))))
 
 (defn- js-opts->cljs-opts [opts]
-  (let [cljs (js->clj opts)
-        bindings (get cljs "bindings")
-        bindings (symbolize-keys bindings)
-        namespaces (get cljs "namespaces")
-        namespaces (map-vals symbolize-keys namespaces)
-        namespaces (symbolize-keys namespaces)
-        allow (get cljs "allow")
+  (let [cljs (obj->clj opts keyword)
+        bindings (get cljs :bindings)
+        bindings (obj->clj bindings symbol)
+        namespaces (get cljs :namespaces)
+        namespaces (obj->clj namespaces symbol)
+        namespaces (map-vals (fn [namespace]
+                               (obj->clj namespace symbol))
+                             namespaces)
+        allow (get cljs :allow)
         allow (mapv symbol allow)
-        deny (get cljs "deny")
+        deny (get cljs :deny)
         deny (mapv symbol deny)
-        realize-max (get cljs "realizeMax")
-        preset (when-let [v (get cljs "preset")]
+        realize-max (get cljs :realizeMax)
+        preset (when-let [v (get cljs :preset)]
                  (keyword v))
-        classes (get cljs "classes")
-        allow-all-classes (get classes "allow")
-        classes (symbolize-keys classes)
+        classes (get cljs :classes)
+        classes (obj->clj classes symbol)
+        allow-all-classes (get classes 'allow)
         classes (if allow-all-classes (assoc classes :allow allow-all-classes) classes)
-        env (get cljs "env")]
+        env (get cljs :env)]
     {:bindings bindings
      :namespaces namespaces
      :allow allow
