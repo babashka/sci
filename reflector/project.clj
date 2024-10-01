7372da25ec6e8b65e140eb62ca12e42bc6babec4
Michiel Borkent <michielborkent@gmail.com>
2024-10-01 11:02:16 +0200

    Reflector: target 1.8

--- reflector/project.clj
+++ reflector/project.clj
@@ -1,8 +1,9 @@
-(defproject borkdude/sci.impl.reflector "0.0.2"
+(defproject borkdude/sci.impl.reflector "0.0.3"
   :dependencies [[org.clojure/clojure "1.9.0"]]
   :description "JVM reflection support for SCI"
   :licence "MIT"
   :java-source-paths ["src"]
+  :javac-options ["-target" "1.8" "-source" "1.8" "-Xlint:-options"]
   :deploy-repositories [["clojars" {:url "https://clojars.org/repo"
                                     :username :env/clojars_user
                                     :password :env/clojars_pass
