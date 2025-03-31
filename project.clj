bb544b70ecdf3574bb7600f9203b08041e9d50d0
Michiel Borkent <michielborkent@gmail.com>
2025-03-31 16:04:32 +0200

    Bump edamame

--- project.clj
+++ project.clj
@@ -11,7 +11,7 @@
   :source-paths ["src"]
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [borkdude/sci.impl.reflector "0.0.3"]
-                 [borkdude/edamame "1.4.28"]
+                 [borkdude/edamame "1.4.29"]
                  [org.babashka/sci.impl.types "0.0.2"]
                  [borkdude/graal.locking "0.0.2"]]
   :plugins [[lein-codox "0.10.7"]]
